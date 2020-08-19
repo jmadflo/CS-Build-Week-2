201e5c07a01e35d81511acf6c14a04c5d1c9182f
Juan Gerardo Madero Flores <juan@Juans-MacBook.attlocal.net>
2020-08-19 16:55:14 -0700

    cleaning algorithm implementation

--- search_in_rotated_sorted_array.py
+++ search_in_rotated_sorted_array.py
@@ -5,21 +5,21 @@ class Solution(object):
         :type target: int
         :rtype: int
         """
-        left = 0
-        right = len(nums) - 1
+        left_side = 0
+        right_side = len(nums) - 1
         pivot_point = -1
         # first find the pivot_point
-        while left < right:
-            if nums[left] > nums[left+1]:
-                pivot_point = left
+        while left_side < right_side:
+            if nums[left_side] > nums[left_side+1]:
+                pivot_point = left_side
                 break
-            if nums[right] < nums[right-1]:
-                pivot_point = right-1
+            if nums[right_side] < nums[right_side-1]:
+                pivot_point = right_side-1
                 break
-            left += 1
-            right -= 1
+            left_side += 1
+            right_side -= 1
         if pivot_point == -1:
-            pivot_point = left
+            pivot_point = left_side
         left_nums = nums[:pivot_point+1]
         right_nums = nums[pivot_point+1:]
         if target in left_nums:
